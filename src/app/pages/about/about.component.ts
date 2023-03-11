03c872c7a112fc33105be68d43b930c4fa0135f3
Cristian Camilo <tiancamilo59@gmail.com>
2023-03-11 13:43:38 -0500

    Creando metodos en el servicio para cargar información del aboutcon informacion de firebase

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
+  constructor( public infoService: InfoPaginaService){}
+
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,30 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) {
 
+    this.cargarInfo();
+    this.cargarEquipo();
+    
+  }
+
+  private cargarInfo(){
     // Leer archivo JSON
     this.http.get('assets/data/data-pagina.json').subscribe((resp: InfoPagina) => {
       this.cargada = true;
       this.info= resp;
-      console.log(resp);
-    })
-
-
+    });
   }
+
+  private cargarEquipo(){
+    // Leer archivo JSON
+    this.http.get('https://angular-ab66f-default-rtdb.firebaseio.com/equipo.json').subscribe((resp: any) => {
+      this.cargada = true;
+      this.equipo= resp;
+      console.log(resp);
+    });
+  }
+
 }
